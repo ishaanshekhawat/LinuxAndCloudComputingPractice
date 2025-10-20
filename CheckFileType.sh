62d1168e4ca708be9a3de9bf3ace00089bb2440e
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-10-20 13:34:13 +0530

    Update CheckFileType.sh

--- CheckFileType.sh
+++ CheckFileType.sh
@@ -3,3 +3,15 @@ read -p "Enter a filename: " thingname
 
 result1=$(find -type f -name "$thingname")        # Look for a regular file with that name.
 result2=$(find -type d -name "$thingname")        # Look for a directory with that name.
+
+# If a file is found, it prints “this is a file.” If a directory is found, it prints “this is a directory.” If neither is found, it prints “this is something else.”
+
+if [ -n "$result1" ]
+then 
+        echo "this is a file"
+elif [ -n "$result2" ]
+then 
+        echo "this is a directory"
+else
+        echo "this is something else"
+fi
